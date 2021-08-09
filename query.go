246bd10fa9af7fba01f4a2ebb5ec2f8794d2c725
Aaron Meier <webgovernor@gmail.com>
2021-08-08 21:08:12 -0600

    Add inline documentation to the Query API

--- query.go
+++ query.go
@@ -15,34 +15,43 @@ type Query struct {
 	OptionalPrefix string
 }
 
+// New returns an instance of Query with a single QueryPart
 func New(text string, args ...interface{}) *Query {
 	q := Q()
 	q.Parts = append(q.Parts, makePart(text, args...))
 	return q
 }
 
+// Q returns a new empty Query
 func Q() *Query {
 	return &Query{}
 }
 
+// Optional returns a query object that has a conditional prefix which only
+// resolves when at least one QueryPart has been added.
 func Optional(prefix string) *Query {
 	return &Query{
 		OptionalPrefix: prefix,
 	}
 }
 
+// And joins the current QueryPart to the previous QueryPart with ' AND '
 func (q *Query) And(text string, args ...interface{}) *Query {
 	return q.Join(" AND ", text, args...)
 }
 
+// Comma joins the current QueryPart to the previous QueryPart with a comma
 func (q *Query) Comma(text string, args ...interface{}) *Query {
 	return q.Join(",", text, args...)
 }
 
+// Concat concatenates the current QueryPart to the previous QueryPart with a
+// zero space string
 func (q *Query) Concat(text string, args ...interface{}) *Query {
 	return q.Join("", text, args...)
 }
 
+// Join joins the current QueryPart to the previous QueryPart with `sep`
 func (q *Query) Join(sep, text string, args ...interface{}) *Query {
 	if len(q.Parts) > 0 {
 		q.Parts = append(q.Parts, makePart(sep+text, args...))
@@ -53,14 +62,17 @@ func (q *Query) Join(sep, text string, args ...interface{}) *Query {
 	return q
 }
 
+// Or joins the current QueryPart to the previous QueryPart with ' OR '
 func (q *Query) Or(text string, args ...interface{}) *Query {
 	return q.Join(" OR ", text, args...)
 }
 
+// Space joins the current QueryPart to the previous QueryPart with a space
 func (q *Query) Space(text string, args ...interface{}) *Query {
 	return q.Join(" ", text, args...)
 }
 
+// Print outputs the sql, parameters, and errors of a Query
 func (q *Query) Print() {
 	sql, params, err := q.ToSql()
 	fmt.Printf("SQL: %v\n", sql)
@@ -74,12 +86,15 @@ func (q *Query) ToMysql() (string, []interface{}, error) {
 	return sql, params, err
 }
 
+// ToPgsql returns the sql placeholders with dollarsign format used by postgres
 func (q *Query) ToPgsql() (string, []interface{}, error) {
 	sql, params, _ := q.toSql()
 	sql, err := dialectReplace(PGSQL, sql, params)
 	return sql, params, err
 }
 
+// ToRaw returns a string which the parameters have been resolved added
+// as correctly as possible.
 func (q *Query) ToRaw() (string, error) {
 	sql, params, _ := q.toSql()
 	sql, err := dialectReplace(RAW, sql, params)
