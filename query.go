22dc9ca6e82ba9964fa0574d85268415413cecb6
Aaron Meier <webgovernor@gmail.com>
2021-09-02 21:50:32 -0600

    add junk function for coverage test

--- query.go
+++ query.go
@@ -36,6 +36,12 @@ func Optional(prefix string) *Query {
 	}
 }
 
+func junk() {
+	if 1 == 2 {
+		return
+	}
+}
+
 // And joins the current QueryPart to the previous QueryPart with ' AND '
 func (q *Query) And(text string, args ...interface{}) *Query {
 	if q == nil {
