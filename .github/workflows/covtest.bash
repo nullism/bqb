000200ca4b199a4c12a48694d8fe6d4ceeb2d4a8
Aaron Meier <webgovernor@gmail.com>
2021-09-03 08:32:56 -0600

    Remove coverage bash script

--- .github/workflows/covtest.bash
+++
@@ -1,8 +0,0 @@
-last_cov=$(go tool cover -func=coverage.out | tail -1 | awk '{ print $3 }' | sed s/%//g)
-go test -coverprofile=coverage.out
-this_cov=$(go tool cover -func=coverage.out | tail -1 | awk '{ print $3 }' | sed s/%//g)
-
-if [ "${last_cov}" -gt "${this_cov}" ]; then
-    echo "Coverage has dropped from ${last_cov} to ${this_cov}"
-    exit 1
-fi
