723560421e55fc15830d58e02012b7c20f1046c5
Aaron Meier <webgovernor@gmail.com>
2023-06-23 09:39:25 -0600

    Move types over

--- utils.go
+++ utils.go
@@ -7,30 +7,6 @@ import (
 	"strings"
 )
 
-// Dialect holds the Query dialect
-type Dialect string
-
-const (
-	// PGSQL postgres dialect
-	PGSQL Dialect = "postgres"
-	// MYSQL MySQL dialect
-	MYSQL Dialect = "mysql"
-	// RAW dialect uses no parameter conversion
-	RAW Dialect = "raw"
-	// SQL generic dialect
-	SQL Dialect = "sql"
-
-	paramPh = "{{xX_PARAM_Xx}}"
-)
-
-// JsonMap is a custom type which tells bqb to convert the parameter to
-// a JSON object without requiring reflection.
-type JsonMap map[string]interface{}
-
-// JsonList is a type that tells bqb to convert the parameter to a JSON
-// list without requiring reflection.
-type JsonList []interface{}
-
 func dialectReplace(dialect Dialect, sql string, params []interface{}) (string, error) {
 	if dialect == MYSQL || dialect == SQL {
 		sql = strings.ReplaceAll(sql, paramPh, "?")
