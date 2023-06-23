d1120d4d553aa54a96473fb5d4f97821966676f2
Aaron Meier <webgovernor@gmail.com>
2023-06-23 09:36:15 -0600

    Update Embedder description

--- types.go
+++ types.go
@@ -1,5 +1,8 @@
 package bqb
 
+// Embedder embeds a value directly into a query string.
+// Note: Since this is embedded and not bound,
+// attention must be paid to sanitizing this input.
 type Embedder interface {
 	RawValue() string
 }
